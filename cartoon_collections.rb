cc2380584857401cc10d71891d2b282bf82e41be
John Pena <jpena91@gmail.com>
2019-02-17 17:05:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,11 +21,11 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?("cheddar")
-    p "cheddar" 
+    return "cheddar" 
     elsif array.include?("gouda")
-    p "gouda"
+    return  "gouda"
   elsif array.include?("camembert")
-    p "camembert"
+    return  "camembert"
 else nil 
 end 
 end
